1c034569203e800e4d42ef94f5ac06a9ff3d39bd
Ramtin Ghassemi <ghassemiramtin@gmail.com>
2026-01-01 15:36:50 -0800

    complete day11

--- 2025/day11/day11.cc
+++ 2025/day11/day11.cc
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <unordered_set>
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <queue>
+#include <stack>
+#include <tuple>
 
 #include "../../file_utils/file_parser.cc"
 #include "../../string_utils/string_parser.cc"
+
+
+struct TupleHash {
+    std::size_t operator()(
+        const std::tuple<std::string, bool, bool>& t
+    ) const {
+        const auto& [s, b1, b2] = t;
+        std::size_t h1 = std::hash<std::string>{}(s);
+        std::size_t h2 = std::hash<bool>{}(b1);
+        std::size_t h3 = std::hash<bool>{}(b2);
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
+    }
+};
+using State = std::tuple<std::string, bool, bool>;
+std::unordered_map<State, uint64_t, TupleHash> cache;
+
+uint64_t Dfs(std::string curr,
+		const std::unordered_map<std::string,
+		std::vector<std::string>>& server_rack,
+		bool has_dac, bool has_fft)
+{
+	if (curr == "out") return has_dac && has_fft ? 1 : 0;
+	if (curr == "dac") has_dac = true;
+	if (curr == "fft") has_fft = true;
+	auto it = server_rack.find(curr);
+	if (it == server_rack.end()) return 0;
+	State state{curr, has_dac, has_fft};
+    	if (auto it = cache.find(state); it != cache.end())
+        	return it->second;
+	uint64_t res = 0;
+	for (const auto& next : it->second)
+	{
+		res += Dfs(next, server_rack, has_dac, has_fft);
+	}
+	return cache[state] = res;
+}
 
 int main()
 {
@@ -20,24 +58,5 @@ int main()
 			server_rack[key].push_back(s);
 		}
 	}
-	std::queue<std::string> q;
-	q.push("you");
-	uint64_t res = 0;
-	while (!q.empty())
-	{
-		std::string curr = q.front();
-		q.pop();
-
-		if (curr == "out")
-		{
-			res++;
-			continue;
-		}
-
-		for (const auto& next : server_rack[curr])
-		{
-			q.push(next);
-		}
-	}
-	std::cout << "Result: " << res << std::endl;
+	std::cout << "Recursive Dfs: " << Dfs("svr", server_rack, false, false) << std::endl;
 }
